04621b8874c0c934021e2638efbc3105954c7761
greenylifeonline-beep <greenylife.online@gmail.com>
2026-07-13 23:59:45 +0200

    Update products.js

--- app/js/modules/products.js
+++ app/js/modules/products.js
@@ -1,23 +1,6 @@
 async function loadProducts() {
 
-    try {
-
-        const response = await fetch("../../data/products.json");
-
-        if (!response.ok) {
-            throw new Error("Failed to load products.json");
-        }
-
-        const products = await response.json();
-
-        console.log("Products Loaded:");
-        console.log(products);
-
-    } catch (error) {
-
-        console.error("Error:", error);
-
-    }
+    alert("products.js loaded");
 
 }
 
